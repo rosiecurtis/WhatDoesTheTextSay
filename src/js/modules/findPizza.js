c9b1d625c7ea03e2d40fc89874846f22f9e6bc0b
rosiecurtis <73502370+rosiecurtis@users.noreply.github.com>
2020-10-27 16:27:14 +0000

    added function to find the word pizza

---
+++ src/js/modules/findPizza.js
@@ -0,0 +1,12 @@
+ 
+
+function render(userText) {
+    var str = "Pizza";
+    var n = str.includes("Pizza")
+  let output = "The word " + str + " is in the text Pizza is Great!!";
+  return output;
+}
+
+
+export { render };
+
